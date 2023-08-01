78dab7efac91fd3d4431822917b6d4469698b8a9
Michel Hathier <mhathiergmail.com>
2023-08-01 18:50:59 +0200

    Add user profile picture field in registration form

--- src/Controller/CoverImageController.php
+++ src/Controller/CoverImageController.php
@@ -2,15 +2,39 @@
 
 namespace App\Controller;
 
+use App\Entity\Picture;
 use App\Entity\Trick;
+use App\Form\CoverImageType;
 use App\Repository\TrickRepository;
+use App\Service\PictureService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CoverImageController extends AbstractController
 {
+    private $pictureService;
+
+    public function __construct(PictureService $pictureService)
+    {
+        $this->pictureService = $pictureService;
+    }
+
+    public function editCoverImage(Trick $trick, Request $request, Picture $picture)
+    {
+        $formCoverImage = $this->createForm(CoverImageType::class, $picture);
+        $formCoverImage->handleRequest($request);
+
+        if ($formCoverImage->isSubmitted() && $formCoverImage->isValid()) {
+            $images = $formCoverImage->get('coverImage')->getData();
+            $this->pictureService->newPicture($trick, $images);
+        }
+
+        return $formCoverImage->createView();
+    }
+
     #[Route('/coverimage/delete/{slug}', name: 'coverimage.delete', methods: ['GET'])]
     public function deleteCoverImage($slug, Trick $trick, Filesystem $filesystem, TrickRepository $trickRepository): Response
     {
--- src/Controller/PictureController.php
+++ src/Controller/PictureController.php
@@ -3,15 +3,39 @@
 namespace App\Controller;
 
 use App\Entity\Picture;
+use App\Entity\Trick;
+use App\Form\EditPictureType;
 use App\Repository\TrickRepository;
+use App\Service\PictureService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PictureController extends AbstractController
 {
+    private $pictureService;
+
+    public function __construct(PictureService $pictureService)
+    {
+        $this->pictureService = $pictureService;
+    }
+
+    public function editPicture(Trick $trick, Request $request, Picture $picture)
+    {
+        $formEditPicture = $this->createForm(EditPictureType::class, $picture);
+        $formEditPicture->handleRequest($request);
+
+        if ($formEditPicture->isSubmitted() && $formEditPicture->isValid()) {
+            $images = $formEditPicture->get('editPicture')->getData();
+            $this->pictureService->newPicture($trick, $images);
+        }
+
+        return $formEditPicture->createView();
+    }
+
     #[Route('/picture/delete/{slug}/{id}', name: 'picture.delete', methods: ['GET'])]
     public function deletePicture($slug, Picture $picture, TrickRepository $trickRepository, Filesystem $filesystem, EntityManagerInterface $manager): Response
     {
